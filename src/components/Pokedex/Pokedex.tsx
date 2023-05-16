93b7f44b618b1bb22ab67260bbf2571c71412681
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-16 12:34:31 -0300

    added no pokemons case

--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -40,16 +40,23 @@ const Pokedex: React.FC<PokedexProps> = (props) => {
         <div className="container pokedex mt-3 pt-2">
           {props.isSearch && <PokeSearch inputChanged={props.inputChanged!} />}
           <div className="row d-flex justify-content-center mt-5">
-            {props.pokemons.map((pokemon, _index) => {
-              return (
-                <div
-                  className="col-12 col-sm-11 col-md-8 col-lg-6"
-                  key={pokemon.name + Math.random()}
-                >
-                  <Card pokemon={pokemon}></Card>
-                </div>
-              );
-            })}
+            {props.pokemons.length > 0 &&
+              props.pokemons.map((pokemon, _index) => {
+                return (
+                  <div
+                    className="col-12 col-sm-11 col-md-8 col-lg-6"
+                    key={pokemon.name + Math.random()}
+                  >
+                    <Card pokemon={pokemon}></Card>
+                  </div>
+                );
+              })}
+
+            {props.pokemons.length === 0 && (
+              <div className="col-12 col-sm-11 col-md-8 col-lg-6 mt-4 text-center font-weight-bold">
+                Nenhum Pokémon encontrado.
+              </div>
+            )}
           </div>
         </div>
       </PokedexWrapper>
